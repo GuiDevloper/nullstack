8a769b3c7af87559b7ae7d6d6ecdc4dafbc223f7
GuiDevloper <guicorrea384@gmail.com>
2022-01-02 18:51:06 -0300

    :label: Add types to Logo component

---
+++ logo.d.ts
@@ -0,0 +1,10 @@
+type NullstackLogo = {
+  light?: boolean
+  monotone?: boolean
+  duotone?: boolean
+  height?: string | number
+}
+
+export default function Logo(
+  {}: NullstackLogo
+): JSX.Element
