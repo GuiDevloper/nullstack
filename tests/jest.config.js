aaecd00a72b3ff32bb04a20e14d6f538a096e87b
GuiDevloper <guicorrea384@gmail.com>
2022-01-19 16:39:46 -0300

    :green_heart: Args to Jest based on env

--- tests/jest.config.js
+++ tests/jest.config.js
@@ -1,5 +1,9 @@
+const CI = Boolean(process.env.CI || false);
+
 module.exports = {
-  "preset": "jest-puppeteer",
-  "testEnvironment": "./test-environment.js",
-  "globalTeardown": "./global-teardown.js"
+  preset: "jest-puppeteer",
+  testEnvironment: "./test-environment.js",
+  globalTeardown: "./global-teardown.js",
+  forceExit: CI,
+  testTimeout: CI ? 5000 : 20000
 }
